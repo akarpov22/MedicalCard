2f1dd175515f2e5e5a44e76d05fa86c1848e57f9
Artem Karpov <artem.kar2003@gmail.com>
2024-12-24 19:36:46 +0200

    fix types

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,11 +1,22 @@
+import { PrismaClient } from '@prisma/client';
+
+type Context = {
+  prisma: PrismaClient;
+};
+
 export const Query = {
-  users: async (_, __, { prisma }) => {
+  users: async (_: unknown, __: unknown, { prisma }: Context) => {
     return prisma.user.findMany();
   },
 };
 
 export const Mutation = {
-  createUser: async (_, { name, email, role }, { prisma }) => {
+  createUser: async (
+    _: unknown,
+    args: { name: string; email: string; role: string },
+    { prisma }: Context
+  ) => {
+    const { name, email, role } = args;
     return prisma.user.create({
       data: { name, email, role },
     });
